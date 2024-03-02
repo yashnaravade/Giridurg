7f87e84eaa036a74103c3f28ab0bb776063f8749
prajaktadharpur28 <prajaktadharpure28@gmail.com>
2024-03-02 14:53:42 +0530

    Added header

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,83 @@
+import React from "react";
+import Logo from "../../assets/logo.png";
+
+function Header() {
+  return (
+    <div>
+      {/* bootstrap navbar with login button, links, and a language toggle, etc */}
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img src={Logo} alt="Bootstrap" />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <div className="mx-auto"> {/* Center the ul */}
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/about">
+                    About
+                  </a>
+                </li>
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Our Rooms
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        AC
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Non-AC
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/gallery">
+                    Gallery
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link"  href="/contactus">
+                    Contact Us
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="ml-auto"> {/* Right-align the login/signup button */}
+              <button className="btn btn-primary">Login</button>
+              <button className="btn btn-secondary ml-2">Signup</button>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
