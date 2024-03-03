8a8c15dda80fdc59b20320d8a4812032ef394b7f
Yash Naravade <yashdip123@gmail.com>
2024-03-03 11:36:05 +0530

    fix: carousel image change

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import banner1 from '../../assets/IMG-20240215-WA0008.jpg';
-import banner2 from '../../assets/IMG-20240215-WA0009.jpg';
-import banner3 from '../../assets/IMG-20240215-WA0010.jpg';
-
+import React, { useEffect, useRef } from "react";
+import banner1 from "../../assets/IMG-20240215-WA0008.jpg";
+import banner2 from "../../assets/IMG-20240215-WA0009.jpg";
+import banner3 from "../../assets/IMG-20240215-WA0010.jpg";
 
 function LandingPage() {
+  const carouselRef = useRef(null);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const nextButton = carouselRef.current.querySelector(
+        ".carousel-control-next"
+      );
+      nextButton.click(); // Simulate a click on the next button
+    }, 3000); // Change slide every 3 seconds
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
   return (
     <div>
-      <div id="carouselExampleCaptions" className="carousel slide">
+      <div
+        id="carouselExampleCaptions"
+        className="carousel slide"
+        ref={carouselRef}
+      >
         <div className="carousel-indicators">
           <button
             type="button"
@@ -32,36 +50,30 @@ function LandingPage() {
         </div>
         <div className="carousel-inner">
           <div className="carousel-item active">
-            <img
-              src={banner1}
-              className="d-block w-100"
-              alt="First slide"
-            />
+            <img src={banner1} className="d-block w-100" alt="First slide" />
             <div className="carousel-caption d-none d-md-block">
               <h5>First slide label</h5>
-              <p>Some representative placeholder content for the first slide.</p>
+              <p>
+                Some representative placeholder content for the first slide.
+              </p>
             </div>
           </div>
           <div className="carousel-item">
-            <img
-              src={banner2}
-              className="d-block w-100"
-              alt="Second slide"
-            />
+            <img src={banner2} className="d-block w-100" alt="Second slide" />
             <div className="carousel-caption d-none d-md-block">
               <h5>Second slide label</h5>
-              <p>Some representative placeholder content for the second slide.</p>
+              <p>
+                Some representative placeholder content for the second slide.
+              </p>
             </div>
           </div>
           <div className="carousel-item">
-            <img
-              src={banner3}
-              className="d-block w-100"
-              alt="Third slide"
-            />
+            <img src={banner3} className="d-block w-100" alt="Third slide" />
             <div className="carousel-caption d-none d-md-block">
               <h5>Third slide label</h5>
-              <p>Some representative placeholder content for the third slide.</p>
+              <p>
+                Some representative placeholder content for the third slide.
+              </p>
             </div>
           </div>
         </div>
@@ -71,7 +83,10 @@ function LandingPage() {
           data-bs-target="#carouselExampleCaptions"
           data-bs-slide="prev"
         >
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
           <span className="visually-hidden">Previous</span>
         </button>
         <button
@@ -80,7 +95,10 @@ function LandingPage() {
           data-bs-target="#carouselExampleCaptions"
           data-bs-slide="next"
         >
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
           <span className="visually-hidden">Next</span>
         </button>
       </div>
