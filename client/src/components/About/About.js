2918660db19d823ebf124ea7acf18e227ab9e44e
Yash Naravade <yashdip123@gmail.com>
2024-03-03 11:56:06 +0530

    fix: about css

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,32 +1,44 @@
 import React from "react";
-import aboutImage from "../../assets/IMG-20240215-WA0025.jpg"; 
+import "./About.css";
+import aboutImage from "../../assets/IMG-20240215-WA0049.jpg";
 
 function About() {
   return (
     <div className="container mt-5">
-      <div className="row">
-        {/* Centered Heading */}
-        <div className="col-12 text-center mb-4">
-          <h2>About Us</h2>
-        </div>
-
-        {/* First Column */}
-        <div className="col-md-6">
-          <p>
-            Welcome to our hotel! We are committed to providing you with a
-            comfortable and memorable stay. Our team is dedicated to ensuring
-            that your experience is nothing short of exceptional.
-          </p>
-          <button className="btn btn-primary">Learn More</button>
-        </div>
-
-        {/* Second Column */}
+      <h2 className="text-center mb-2">About Us</h2>
+      <div className="underline mb-4"></div>
+      <div className="row align-items-center">
+        {/* Image Column */}
         <div className="col-md-6">
           <img
             src={aboutImage}
             alt="About Us"
             className="img-fluid rounded"
+            style={{ maxWidth: "100%", height: "auto" }}
           />
+        </div>
+
+        {/* Text Column */}
+        <div className="col-md-6">
+          <div className="text-center text-md-start">
+            <p>
+              Welcome to our hotel near Shivaji Maharaj's fort "Pargad"! We
+              offer unmatched services and are committed to providing you with a
+              comfortable and memorable stay. Our team is dedicated to ensuring
+              that your experience is nothing short of exceptional.
+            </p>
+            <p>
+              Our hotel is located in the picturesque surroundings of Shivaji
+              Maharaj's fort "Pargad," offering breathtaking views and a
+              peaceful atmosphere. Whether you're here for business or leisure,
+              we strive to make your stay unforgettable.
+            </p>
+            <p>
+              Experience our hospitality and discover the beauty of the region.
+              Book your stay with us today!
+            </p>
+            <button className="btn btn-primary">Learn More</button>
+          </div>
         </div>
       </div>
     </div>
