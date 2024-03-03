1a99b2f6d79f9d2bb3b6bf03abd4af587b0f202c
Yash Naravade <yashdip123@gmail.com>
2024-03-03 12:04:50 +0530

    fix: gallery css

--- client/src/components/Gallery/Gallery.js
+++ client/src/components/Gallery/Gallery.js
@@ -1,19 +1,22 @@
 import React from "react";
-import galleryImage1 from "../../assets/IMG-20240215-WA0070.jpg"; // Replace with the actual path to your image
-import galleryImage2 from "../../assets/IMG-20240215-WA0070.jpg"; // Replace with the actual path to your image
-import galleryImage3 from "../../assets/IMG-20240215-WA0070.jpg"; // Replace with the actual path to your image
-import galleryImage4 from "../../assets/IMG-20240215-WA0070.jpg"; // Replace with the actual path to your image
-import galleryImage5 from "../../assets/IMG-20240215-WA0070.jpg"; // Replace with the actual path to your image
-import galleryImage6 from "../../assets/IMG-20240215-WA0070.jpg"; // Replace with the actual path to your image
-import galleryImage7 from "../../assets/IMG-20240215-WA0070.jpg"; // Replace with the actual path to your image
-import galleryImage8 from "../../assets/IMG-20240215-WA0070.jpg"; // Replace with the actual path to your image
+import "./Gallery.css";
+import galleryImage1 from "../../assets/IMG-20240215-WA0070.jpg";
+import galleryImage2 from "../../assets/IMG-20240215-WA0071.jpg";
+import galleryImage3 from "../../assets/IMG-20240215-WA0072.jpg";
+import galleryImage4 from "../../assets/IMG-20240215-WA0073.jpg";
+import galleryImage5 from "../../assets/IMG-20240215-WA0074.jpg";
+import galleryImage6 from "../../assets/IMG-20240215-WA0075.jpg";
+import galleryImage7 from "../../assets/IMG-20240215-WA0078.jpg";
+import galleryImage8 from "../../assets/IMG-20240215-WA0077.jpg";
 
 function Gallery() {
   return (
     <div className="container mt-5">
       <div className="text-center mb-4">
         <h2>Gallery</h2>
-        <p>Explore our gallery showcasing memorable moments and experiences.</p>
+        <p>
+          Explore our gallery showcasing breathtaking views near Pargad fort.
+        </p>
       </div>
 
       <div className="row">
@@ -21,56 +24,56 @@ function Gallery() {
           <img
             src={galleryImage1}
             alt="Gallery Image 1"
-            className="img-fluid rounded"
+            className="img-fluid rounded gallery-image"
           />
         </div>
         <div className="col-md-3 mb-4">
           <img
             src={galleryImage2}
             alt="Gallery Image 2"
-            className="img-fluid rounded"
+            className="img-fluid rounded gallery-image"
           />
         </div>
         <div className="col-md-3 mb-4">
           <img
             src={galleryImage3}
             alt="Gallery Image 3"
-            className="img-fluid rounded"
+            className="img-fluid rounded gallery-image"
           />
         </div>
         <div className="col-md-3 mb-4">
           <img
             src={galleryImage4}
             alt="Gallery Image 4"
-            className="img-fluid rounded"
+            className="img-fluid rounded gallery-image"
           />
         </div>
         <div className="col-md-3 mb-4">
           <img
             src={galleryImage5}
             alt="Gallery Image 5"
-            className="img-fluid rounded"
+            className="img-fluid rounded gallery-image"
           />
         </div>
         <div className="col-md-3 mb-4">
           <img
             src={galleryImage6}
             alt="Gallery Image 6"
-            className="img-fluid rounded"
+            className="img-fluid rounded gallery-image"
           />
         </div>
         <div className="col-md-3 mb-4">
           <img
             src={galleryImage7}
             alt="Gallery Image 7"
-            className="img-fluid rounded"
+            className="img-fluid rounded gallery-image"
           />
         </div>
         <div className="col-md-3 mb-4">
           <img
             src={galleryImage8}
             alt="Gallery Image 8"
-            className="img-fluid rounded"
+            className="img-fluid rounded gallery-image"
           />
         </div>
       </div>
